d42d876ba5168c446ebaa28419a0797e8d6f944e
Perry Shelton <sellwithperry@gmail.com>
2022-04-20 10:17:55 -0700

    Add basic bootstrap template

--- src/main/java/com/pbk/flights/Controllers/MyController.java
+++ src/main/java/com/pbk/flights/Controllers/MyController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 public class MyController {
 
     @Autowired
@@ -16,27 +17,27 @@ public class MyController {
     @Autowired
     private UserServices userServices;
 
-    @GetMapping("/Flights")
+    @GetMapping("/flights")
     public List<Flight> getAllFlight(){
         return this.flightService.getAllFlights();
     }
 
     @GetMapping("/Flights/{flightID}")
-    public Flight getFlight(@PathVariable String courseID){
-        return this.flightService.getFlightByID(Integer.parseInt(courseID));
+    public Flight getFlight(@PathVariable String flightID){
+        return this.flightService.getFlightByID(Integer.parseInt(flightID));
     }
 
-    @PostMapping("/Flights")
+    @PostMapping("/flights")
     public Flight addFlight(@RequestBody Flight flight){
         return this.flightService.addFlight(flight);
     }
 
-    @PutMapping("/Flights")
+    @PutMapping("/flights")
     public boolean updateFlight(@RequestBody Flight flight){
         return this.flightService.updateFlight(flight);
     }
 
-    @DeleteMapping("/Flights/{flightID}")
+    @DeleteMapping("/flights/{flightID}")
     public String removeFlight(@PathVariable String flightID){
         return String.valueOf(this.flightService.getFlightByID(Integer.parseInt(flightID)));
     }
@@ -44,29 +45,29 @@ public class MyController {
 
     //Users
 
-    @GetMapping("/Users")
+    @GetMapping("/users")
     public List<User> getUsers(){
         return this.userServices.getUsers();
     }
 
 // Need to send an integer as ID
-    @GetMapping("/Users/{userID}")
+    @GetMapping("/users/{userID}")
     public User getUser(@PathVariable String userID){
         return this.userServices.getUser(Integer.parseInt(userID));
     }
 
 
-    @PostMapping("/Users")
+    @PostMapping("/users")
     public boolean addUser(@RequestBody User user){
         return this.userServices.addUser(user);
     }
 
-    @PutMapping("/Users")
+    @PutMapping("/users")
     public boolean updateUser(@RequestBody User user){
         return this.userServices.updateUser(user);
     }
 
-    @DeleteMapping("/Users/{userID}")
+    @DeleteMapping("/users/{userID}")
     public String removeUser(@PathVariable String userID){
         //return String.valueOf(this.userServices(Integer.parseInt(userID)));
         return String.valueOf(this.userServices.getUser(Integer.parseInt(userID)));
